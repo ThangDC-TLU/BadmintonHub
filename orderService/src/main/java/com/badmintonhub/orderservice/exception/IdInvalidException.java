39ba68efc046fc089390792356f86faf8b5ede89
ThangDC-TLU <dinhcaothang08102004@gmail.com>
2025-08-23 09:28:24 +0700

    config orderService

---
+++ orderService/src/main/java/com/badmintonhub/orderservice/exception/IdInvalidException.java
@@ -0,0 +1,7 @@
+package com.badmintonhub.orderservice.exception;
+
+public class IdInvalidException extends Exception {
+    public IdInvalidException(String message) {
+        super(message);
+    }
+}
